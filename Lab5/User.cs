f094fb617d8189e41e644cb6f6be60caa573c463
Elvis <elvostudios@hotmail.com>
2019-10-11 12:32:34 +0200

    Created user class

---
+++ Lab5/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab5
+{
+    class User
+    {
+        private string name;
+        private string email;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
